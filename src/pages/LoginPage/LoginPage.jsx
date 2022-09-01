4c06544d8fc97761c82d59de13ee23aca3f4cbd9
Anthony Agyekum Mensah <tony.m3nsa@gmail.com>
2022-09-01 00:11:46 +0000

    Implemented the following features: - Set user's username in local storage - Direct user to chat page from login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,13 +2,17 @@ import React, { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
+import { AUTHOR } from "../../constants/constants";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
 
   const onSubmit = (event) => {
     event.preventDefault();
-    navigate("/chat");
+    if (event.target.username.value) {
+      localStorage.setItem(AUTHOR, event.target.username.value);
+      navigate("/chat");
+    }
   };
   return (
     <Fragment>
