1fa3358d36b7ce6f165f59db252930e7661703de
Anthony Agyekum Mensah <tony.m3nsa@gmail.com>
2022-09-01 09:42:17 +0000

    Chat input implemented

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,5 +1,28 @@
 import React, { Fragment } from "react";
+import { Input } from "../../components/Input/Input";
+import { Button } from "../../components/Button/Button";
+import "./chatPage.css";
 
 export const ChatPage = () => {
-  return <Fragment>Chat</Fragment>;
+  return (
+    <Fragment>
+      <div className="chat-area"></div>
+      <form>
+        <div className="messaging-section ">
+          <div className="grid">
+            <div>
+              <Input
+                name="chat"
+                placeholder="Enter text to send"
+                className="full-width"
+              />
+            </div>
+            <div className="text-align-right">
+              <Button type="submit">Send</Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Fragment>
+  );
 };
